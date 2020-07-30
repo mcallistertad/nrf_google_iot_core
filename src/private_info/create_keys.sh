07d02d0d4d77c600e94406dfb93e7896cfe59b12
Tad McAllister <tadmcallister@Tads-MacBook-Air.local>
2020-07-30 18:02:25 +0100

    updated shell script to cleanup keydir

--- src/private_info/create_keys.sh
+++ src/private_info/create_keys.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+read -p "Do you want to remove old key files? " -n 1 -r
+echo
+
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+        read -p "Are you sure? " -n 1 -r
+        echo
+        if [[ $REPLY =~ ^[Yy]$ ]]
+        then
+        dir=$(pwd)
+        find $dir -type f -name "*.pem" -exec rm -rf {} \;
+        fi
+fi
+
+rm -f key.c
+
 read -p 'Enter Device Name: ' devicename
 echo
 
