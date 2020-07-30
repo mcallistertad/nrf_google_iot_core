743ba9fe5f4c98e9c18bc3b3ddcc1618ff632625
Tad McAllister <tadmcallister@Tads-MacBook-Air.local>
2020-07-30 18:10:26 +0100

    modified shell script output

--- src/private_info/create_keys.sh
+++ src/private_info/create_keys.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-read -p "Do you want to remove old key files? " -n 1 -r
+read -p "Do you want to remove old secrets? " -n 1 -r
 echo
 
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
         read -p "Are you sure? " -n 1 -r
         echo
+        echo "Removing old secrets\n"
         if [[ $REPLY =~ ^[Yy]$ ]]
         then
-        dir=$(pwd)
-        find $dir -type f -name "*.pem" -exec rm -rf {} \;
+                dir=$(pwd)
+                find $dir -type f -name "*.pem" -exec rm -rf {} \;
+                echo "Removing key.c\n"
+                rm -f key.c
         fi
 fi
-
-rm -f key.c
 
 read -p 'Enter Device Name: ' devicename
 echo
@@ -24,6 +25,7 @@ echo
 
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
+        echo "Creating secrets"
         python3 create_keys.py -d $devicename -e
         echo Created $devicename certificates
         exit 1
