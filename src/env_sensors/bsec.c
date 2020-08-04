88e129174aaaf53584b11445594abd14d0b1c272
Tad McAllister <tadmcallister@Tads-MacBook-Air.local>
2020-08-04 19:20:06 +0100

    changed bsec sample rate

--- src/env_sensors/bsec.c
+++ src/env_sensors/bsec.c
@@ -25,7 +25,7 @@ LOG_MODULE_REGISTER(bsec, CONFIG_GCLOUD_LOG_LEVEL);
  * BSEC_SAMPLE_RATE_ULP = 0.0033333 Hz = 300 second interval
  * BSEC_SAMPLE_RATE_LP = 0.33333 Hz = 3 second interval
  */
-#define BSEC_SAMPLE_RATE	BSEC_SAMPLE_RATE_LP
+#define BSEC_SAMPLE_RATE	BSEC_SAMPLE_RATE_ULP
 
 /* @breif Interval for saving BSEC state to flash
  *
